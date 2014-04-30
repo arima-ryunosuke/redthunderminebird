180776c7899a1613a3a3e2f6deeec8b74feb77e1
ryunosuke <ryunosuke.arima@gmail.com>
2014-04-30 12:04:48 +0900

    sedに+なんて無い

--- build.sh
+++ build.sh
@@ -14,8 +14,8 @@ cp -r $CURRENT/modules $TEMP/modules
 cp -r $CURRENT/chrome.manifest $TEMP/chrome.manifest
 cp -r $CURRENT/install.rdf $TEMP/install.rdf
 
-sed -i -e "s%var DEBUG = .+;%var DEBUG = false;%g" $TEMP/modules/common.js
-sed -i -e "s%var LOGLEVEL = .+;%var LOGLEVEL = 2;%g" $TEMP/modules/common.js
+sed -i -e "s%var DEBUG = .*;%var DEBUG = false;%g" $TEMP/modules/common.js
+sed -i -e "s%var LOGLEVEL = .*;%var LOGLEVEL = 2;%g" $TEMP/modules/common.js
 
 cd $TEMP
 zip -r $CURRENT/redthunderminebird.xpi ./
